4fcb0a4a6f867f4a680f8f1da89b41da8adb0008
Babak Rahimi <87151913+brahimia@users.noreply.github.com>
2025-09-19 12:20:09 -0400

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,6 +0,0 @@
-import { defineConfig } from 'vite'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  base: '/ServerHub/',   // 👈 IMPORTANT: must match your repo name with trailing slash
-})
