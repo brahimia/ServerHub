4ae6b19b33803c169c99fcd1b0a27c66ea3c66c3
Babak Rahimi <87151913+brahimia@users.noreply.github.com>
2025-09-19 12:16:14 -0400

    Create vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,6 @@
+import { defineConfig } from 'vite'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: '/ServerHub/',   // 👈 IMPORTANT: must match your repo name with trailing slash
+})
